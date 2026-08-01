1f5d2e7262f16d1956c382af4452769498ff981d
vinay jadhav <vinayjadhav969@gmail.com>
2026-08-01 13:15:10 +0530

    feat: modified security_group main.tf file

--- aws/modules/networking/security_groups/main.tf
+++ aws/modules/networking/security_groups/main.tf
@@ -1,8 +1,8 @@
 resource "aws_security_group" "main" {
+    for_each = var.security_groups
     name = each.key
     description = each.value.description
     vpc_id = var.vpc_id
-    for_each = var.security_groups
     
     tags = merge(
         each.value.tags,
